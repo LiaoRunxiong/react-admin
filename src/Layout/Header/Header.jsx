e4d1d5d1c58fefc4128954aaf1861df9b327f36c
LiaoRunxiong <958570363@qq.com>
2023-11-06 09:17:36 +0800

    第一次提交！！

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
-import { useState } from "react";
-function App() {
-  const [num, setNum] = useState(0);
-  const handleClick = (e: any) => {
-    console.log(9, e);
 
-    setNum((v) => v + 1);
-  };
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button type="primary" onClick={handleClick}>
-          Button
-        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.数量：{num}
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
